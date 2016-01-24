ca6ecdcf513a243ab365e4dc783c516377b3c664
Alexander Kazakov <kazakov.a.s.90@gmail.com>
2016-01-24 13:52:24 -0800

    feature: add new tasks

--- front/app/src/tasks/TaskController.js
+++ front/app/src/tasks/TaskController.js
@@ -3,13 +3,16 @@
    angular
           .module('tasks')
           .controller('TaskController', [
-             'taskService', '$log', '$q',
+             'taskService', '$scope', '$log', '$q', 
               TaskController
           ]);
 
 
-   function TaskController(taskService, $log, $q) {
+   function TaskController(taskService, $scope, $log, $q) {
       var self = this;
+      self.scope = $scope;
+      self.addNewTask = addNewTask;
+      self.taskService = taskService;
 
       taskService.loadAllTasks()
 		.then(function(tasks) {
@@ -17,6 +20,13 @@
 		});
 
    }
+
+   function addNewTask() {
+      var newText = this.scope.newTaskText;
+      var newTask = this.taskService.createNewTask(newText);
+      this.tasks.unshift(newTask);
+   }
+
 })();
 
       
