24c9b8fe846088a8a47d3be0979caddf33a64b1e
evgeny-levin <evgeny@atomnas.ru>
2016-01-24 23:09:04 +0400

    [+] Добавлены Классы данных для API.

---
+++ mobile/Windows/HelpPen/HelpPen.Client.Common/Model/API/TaskState.cs
@@ -0,0 +1,23 @@
+﻿namespace HelpPen.Client.Common.Model.API
+{
+	/// <summary>
+	/// Состояние задачи.
+	/// </summary>
+	public enum TaskState
+	{
+		/// <summary>
+		/// Задача не завершена.
+		/// </summary>
+		NotCompleted = 1,
+
+		/// <summary>
+		/// Задача завершена.
+		/// </summary>
+		Completed = 2,
+
+		/// <summary>
+		/// Задача отложена.
+		/// </summary>
+		Stash = 3
+	}
+}
---
+++ mobile/Windows/HelpPen/HelpPen.Client.Common/Model/API/User.cs
@@ -0,0 +1,13 @@
+﻿namespace HelpPen.Client.Common.Model.API
+{
+    /// <summary>
+    /// Пользователь системы.
+    /// </summary>
+	public class User
+	{
+        /// <summary>
+        /// Имя пользователя.
+        /// </summary>
+        public string Login { get; set; }
+    }
+}
