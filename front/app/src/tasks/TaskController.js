0ba50ce894e39a07ca567b85b0e89777250f7123
Alexander Kazakov <kazakov.a.s.90@gmail.com>
2016-01-25 10:33:53 -0800

    [front] remove and move up tasks

--- front/app/src/tasks/TaskController.js
+++ front/app/src/tasks/TaskController.js
@@ -13,6 +13,8 @@
       self.scope = $scope;
       self.addNewTask = addNewTask;
       self.taskService = taskService;
+      self.moveUp = moveUp;
+      self.remove = remove;
 
       taskService.loadAllTasks()
 		.then(function(tasks) {
@@ -25,6 +27,35 @@
       var newText = this.scope.newTaskText;
       var newTask = this.taskService.createNewTask(newText);
       this.tasks.unshift(newTask);
+   }
+
+   function moveUp(id) {
+      this.taskService.moveUp(id); 
+            var i  = 0;
+            for (var t in this.tasks) {
+               if (this.tasks[t].id == id) {
+                  var task = this.tasks[t];
+                  this.tasks.splice(i, 1);
+                  this.tasks.splice(i -1, 0, task);
+                  return;
+               }
+               i++;
+            }
+
+   }
+   
+   function remove(id) {
+      this.taskService.moveUp(id); 
+            var i  = 0;
+            for (var t in this.tasks) {
+               if (this.tasks[t].id == id) {
+                  var task = this.tasks[t];
+                  this.tasks.splice(i, 1);
+                  return;
+               }
+               i++;
+            }
+
    }
 
 })();
--- front/app/src/tasks/TaskService.js
+++ front/app/src/tasks/TaskService.js
@@ -28,8 +28,8 @@
          if (typeof tasks === 'undefined' || tasks.length == 0) return 0;
 
          var maxId = tasks[0].id;
-         for (var task in tasks) {
-            if (task.id > maxId) maxId = taskId;
+         for (var i in tasks) {
+            if (tasks[i].id > maxId) maxId = tasks[i].id;
          }
          return maxId;
       }
@@ -43,7 +43,19 @@
             var newTask = {id: newId, text: newText, isDone: false};  
             tasks.unshift(newTask);            
             return newTask; 
-         }
+         },
+         moveUp: function (id) {
+            var i  = 0;
+            for (var t in tasks) {
+               if (tasks[t].id == id) {
+                  var task = tasks[t];
+                  tasks.slice(i, 1);
+                  tasks.splice(i -1, 0, task);
+                  return;
+               }
+               i++;
+            }
+         }   
       };     
    }
 })();
