673de50de286db1779fcee06f1deea1cfe51343a
Alexander Kazakov <kazakov.a.s.90@gmail.com>
2016-02-01 00:40:22 +0300

    [BACK] Security

--- back/src/main/java/com/helppen/service/ContextBasedUserService.java
+++ back/src/main/java/com/helppen/service/ContextBasedUserService.java
@@ -1,11 +1,17 @@
 package com.helppen.service;
 
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 
 public class ContextBasedUserService  implements UserService {
 
+    private static final String ANONYMOUS = "anonymous";
+
     @Override
     public String getUserName() {
-        return null;
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (!authentication.isAuthenticated()) return ANONYMOUS;
+
+        return authentication.getName();
     }
 }
