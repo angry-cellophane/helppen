1cde5227b9263f4e79e05fa26e2dc65bc750299a
Alexander Kazakov <kazakov.a.s.90@gmail.com>
2016-04-11 11:42:28 -0700

    [DATABASE] change passwords

--- database/insertDefaultUsers.sql
+++ database/insertDefaultUsers.sql
@@ -2,25 +2,25 @@ USE hpapp;
 INSERT INTO user(id, login, password_hash, username) VALUES (
   1,
   'Alex',
-  '42d077ba26d88ff4',
+  '73dd61b16bc79ce3eef10a5a7c',
   'Alex'
 );
 INSERT INTO user(id, login, password_hash, username) VALUES (
   2,
   'Nikolay',
-  '42d077ba26d88ff4',
+  '7cd86fa63dd684aaede7165b6ff462b2',
   'Nikolay Nuyakshin'
 );
 INSERT INTO user(id, login, password_hash, username) VALUES (
   3,
   'Pavel',
-  '42d077ba26d88ff4',
+  '62d072ac3d8d8df1eef512476aff',
   'Pavel Petyanov'
 );
 INSERT INTO user(id, login, password_hash, username) VALUES (
   4,
   'Eugeny',
-  '42d077ba26d88ff4',
+  '77c463ac3fcec7e0fcf5165f77e974',
   'Eugeny Levin'
 );
 commit;
