2c381d59ef6f19a4b98429c1db0c5df03ebf92df
Alexander Kazakov <kazakov.a.s.90@gmail.com>
2016-04-14 13:18:41 -0700

    [ARCHIVER] runArchiver added

--- web/runArchive.sh
+++ web/runArchive.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-export NODE_PATH="$(pwd)"
-node archiver.js | tee ./logs/archiver.$(date +%Y_%m_%d_%H_%M).log
+export NODE_PATH="$(dirname "$0")"
+node "${NODE_PATH}/archiver.js" | tee "${NODE_PATH}/logs/archiver.$(date +%Y_%m_%d_%H_%M).log"
