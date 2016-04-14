c9c30f21cf14e1377c07c3d245e2ad0be285713a
Alexander Kazakov <kazakov.a.s.90@gmail.com>
2016-04-14 13:28:55 -0700

    [DATABASE] lastChangeDateTime to tasks added

--- database/taskTable.sql
+++ database/taskTable.sql
@@ -6,6 +6,7 @@ CREATE TABLE IF NOT EXISTS task(
   ownerId mediumint(9) NOT NULL,
   orderNumber int(11) NOT NULL,
   creationDateTime datetime not null,
+  lastChangeDateTime datetime not null,
   FOREIGN KEY (ownerId)
         REFERENCES user(id)
         ON DELETE CASCADE
