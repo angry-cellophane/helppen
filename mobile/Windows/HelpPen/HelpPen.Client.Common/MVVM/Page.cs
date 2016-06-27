5959dcf2f8dd75ddf43fc3909aeae84fe473c7ac
evgeny-levin <evgeny@atomnas.ru>
2016-06-27 23:21:21 +0400

    [~] Забытые файлы.

---
+++ mobile/Windows/HelpPen/HelpPen.Client.Common/MVVM/Page.cs
@@ -0,0 +1,133 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+
+using Microsoft.Practices.ServiceLocation;
+using Microsoft.Practices.Unity;
+
+namespace HelpPen.Client.Common.MVVM
+{
+	/// <summary>
+	///     Страница <see cref="Windows.UI.Xaml.Controls.Page" /> для исполщьзования в модели MVVM.
+	/// </summary>
+	public class Page : Windows.UI.Xaml.Controls.Page
+	{
+		#region Constructors and Destructors
+
+		/// <summary>
+		///     Инициализирует новый экзмепляр класса <see cref="Page" />.
+		/// </summary>
+		public Page()
+		{
+			Loading += OnLoading;
+			Loaded += OnLoaded;
+
+			//EnsureViewModelCreated();
+		}
+
+		#endregion
+
+		#region Properties
+
+		/// <summary>
+		///     Модель представления данной страницы.
+		/// </summary>
+		public PageViewModel ViewModel
+		{
+			get
+			{
+				return (PageViewModel)DataContext;
+			}
+			set
+			{
+				DataContext = value;
+			}
+		}
+
+		#endregion
+
+		#region Methods
+
+		/// <summary>
+		///     Создает новый экземпляр модели представления.
+		/// </summary>
+		/// <returns>Созданный экземпляр модели представления.</returns>
+		protected TViewMovel CreateInstance<TViewMovel>()
+			where TViewMovel : PageViewModel
+		{
+			IUnityContainer unityContainer = ServiceLocator.Current.GetInstance<IUnityContainer>();
+
+			return unityContainer.Resolve<TViewMovel>(new ParameterOverride(@"frame", Frame));
+		}
+
+		/// <summary>
+		///     Создает модель пердставления для данной страницы.
+		/// </summary>
+		/// <returns>Созданный экземпляр модели представления.</returns>
+		protected virtual PageViewModel CreateViewModel()
+		{
+			return CreateInstance<EmptyViewModel>();
+		}
+
+		protected virtual void OnLoaded(object sender, RoutedEventArgs routedEventArgs)
+		{
+		}
+
+		protected virtual void OnLoading(FrameworkElement sender, object args)
+		{
+		}
+
+		protected override void OnNavigatedFrom(NavigationEventArgs e)
+		{
+			EnsureViewModelCreated();
+
+			base.OnNavigatedFrom(e);
+
+			ViewModel.OnNavigatedFrom(e);
+		}
+
+		protected override void OnNavigatedTo(NavigationEventArgs e)
+		{
+			EnsureViewModelCreated();
+
+			base.OnNavigatedTo(e);
+
+			ViewModel.OnNavigatedTo(e);
+		}
+
+		protected override void OnNavigatingFrom(NavigatingCancelEventArgs e)
+		{
+			EnsureViewModelCreated();
+
+			base.OnNavigatingFrom(e);
+
+			ViewModel.OnNavigatingFrom(e);
+		}
+
+		private void EnsureViewModelCreated()
+		{
+			if (ViewModel == null || ViewModel is EmptyViewModel)
+			{
+				ViewModel = CreateViewModel();
+			}
+		}
+
+		#endregion
+
+		#region Nested Types
+
+		private sealed class EmptyViewModel : PageViewModel
+		{
+			#region Constructors and Destructors
+
+			public EmptyViewModel(Frame frame)
+				: base(frame)
+			{
+			}
+
+			#endregion
+		}
+
+		#endregion
+	}
+}
---
+++ mobile/Windows/HelpPen/HelpPen.Client.Common/Tracing/TraceableAuthService.cs
@@ -0,0 +1,103 @@
+﻿using System.Diagnostics;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+
+using HelpPen.Client.Common.Model.API;
+
+using Task = System.Threading.Tasks.Task;
+
+namespace HelpPen.Client.Common.Tracing
+{
+	/// <summary>
+	///     Трассируемый сервис аутентификации.
+	/// </summary>
+	public sealed class TraceableAuthService : IAuthService
+	{
+		#region Fields
+
+		private readonly IAuthService _authService;
+
+		#endregion
+
+		#region Constructors and Destructors
+
+		public TraceableAuthService(IAuthService authService)
+		{
+			_authService = authService;
+		}
+
+		#endregion
+
+		#region Properties
+
+		/// <summary>
+		///     Удостоверение пользователя.
+		/// </summary>
+		public ICredentials Credentials
+		{
+			get
+			{
+				return _authService.Credentials;
+			}
+		}
+
+		/// <summary>
+		///     Содержит текущую сессию работы системы или <c>null</c>, если не была выполнена процедура входа.
+		/// </summary>
+		public Session CurrentSession
+		{
+			get
+			{
+				return _authService.CurrentSession;
+			}
+		}
+
+		#endregion
+
+		#region Public Methods and Operators
+
+		/// <summary>
+		///     Осуществляет процедуру аутентификации по заданным <see cref="ICredentials" />.
+		/// </summary>
+		/// <param name="credentials">Удостоврение пользователя.</param>
+		/// <param name="cancellationToken">Токен отмены.</param>
+		/// <returns>Возвращает задачу <see cref="Task{TResult}" />,  в рамках которой происходит получение <see cref="Session" />.</returns>
+		/// <exception cref="AuthenticationException">
+		///     Инициируется при невозможности получить сессию <see cref="Session" /> для
+		///     указанного удостоверения <paramref name="credentials" />.
+		/// </exception>
+		public async Task<Session> Login(ICredentials credentials, CancellationToken cancellationToken)
+		{
+			Stopwatch stopwatch = Stopwatch.StartNew();
+
+			Session session;
+
+			try
+			{
+				session = await _authService.Login(credentials, cancellationToken);
+			}
+			finally
+			{
+				stopwatch.Stop();
+
+				AuthEventSource.Instance.LoginTime(AuthService.AUTH_TOKEN_SERVICE_URI, (uint)stopwatch.ElapsedMilliseconds);
+			}
+
+			return session;
+		}
+
+		/// <summary>
+		///     Осуществляет процедуру завершения актуальности сессии работы пользователя по его инициативе.
+		/// </summary>
+		/// <param name="session">Сессия работы с пользователем.</param>
+		/// <param name="cancellationToken">Токен отмены.</param>
+		/// <returns>Возвращает задачу <see cref="Task{TResult}" />,  в рамках которой происходит завершение работы.</returns>
+		public Task Logout(Session session, CancellationToken cancellationToken)
+		{
+			return _authService.Logout(session, cancellationToken);
+		}
+
+		#endregion
+	}
+}
