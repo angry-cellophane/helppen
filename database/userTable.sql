6c37ac994819a5fbe99a144e3a066f3d273362f3
Alexander Kazakov <kazakov.a.s.90@gmail.com>
2016-11-20 11:50:50 -0800

    [DATABASE] Migration on flyway. Old user table creating script removed.

--- database/userTable.sql
+++
@@ -1,7 +0,0 @@
-USE hpapp;
-CREATE TABLE IF NOT EXISTS user(
-  id mediumint(9) PRIMARY KEY,
-  login varchar(50) NOT NULL,
-  password_hash varchar(300) NOT NULL,
-  username varchar(300) NOT NULL
-);
